3d1eb104d364ae00f3ff44a5f2a590824ccbeffc
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2022-11-12 16:59:53 +0000

    added routes to the main.py

--- backend/main.py
+++ backend/main.py
@@ -18,3 +18,14 @@ app.add_middleware(
 def read_root():
     return {"Ping":"Pong"}
 
+@app.get("/api/level")
+async def get_level():
+    return 1
+
+@app.get("/api/level{id}")
+async def get_level_by_id(id):
+    return 1
+
+@app.post("/api/level")
+async def post_level(level):
+    return 1
