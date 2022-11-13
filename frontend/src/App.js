b3768be069a07a0eb46962a2222ec01ae0960b33
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2022-11-13 11:22:36 +0000

    minor change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,30 +5,29 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <div className='App list-group-item justify-content-center
-      aligh-items-center mx-auto' style={{"width":"400px",
-      "backgroundColor":"white", "marginTop":"15px"}}>
-        <h1 className="card text-white bg-primary mb-1"
-        styleName="max-width: 20rem;">GARBAGE LEVEL DETECTOR</h1>
-        <h6 className="card text-white bg-primary mb-3">
-          USING FARM STACK</h6>
-        <div className="card-body">
-          <h5 className="card text-white bg-dark mb-3">Add A Garbage Bin</h5>
-          <span className="card-text">
-            <input className="mb-2 form-control idIn" placeholder="ID"/>
-            <input className="mb-2 form-control desIn" placeholder="LEVEL"/>
+    <div className='App list-group-item justify-content-center
+    aligh-items-center mx-auto' style={{"width":"400px",
+    "backgroundColor":"white", "marginTop":"15px"}}>
+      <h1 className="card text-white bg-primary mb-1"
+      styleName="max-width: 20rem;">GARBAGE LEVEL DETECTOR</h1>
+      <h6 className="card text-white bg-primary mb-3">
+        USING FARM STACK</h6>
+      <div className="card-body">
+        <h5 className="card text-white bg-dark mb-3">Add A Garbage Bin</h5>
+        <span className="card-text">
+          <input className="mb-2 form-control idIn" placeholder="ID"/>
+          <input className="mb-2 form-control desIn" placeholder="LEVEL"/>
 
-            <button className="btn btn-outline-primary mx-2 mb-3" style={
-            {'borderRadius':"50px", "font-weight":"bold"}}>Add Bin</button>
-          </span>
+          <button className="btn btn-outline-primary mx-2 mb-3" style={
+          {'borderRadius':"50px", "font-weight":"bold"}}>Add Bin</button>
+        </span>
 
-          <h5 className="card text-white bg-dark mb-3">Your Bins</h5>
-          <div>
-            
-          </div>
+        <h5 className="card text-white bg-dark mb-3">Your Bins</h5>
+        <div>
+          {/*TODO - Add items*/}
         </div>
       </div>
+      <h6 className="card text-dark bg-warning py-1 mb-0">Copyright 2022, All rights reserved &copy;</h6>
     </div>
   );
 }
