9df09bc17f31ff3bdefdc0c578b16146042f7368
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2022-11-13 11:18:33 +0000

    Worked on App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,29 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <div className="App">
-      Hello, World!
+      <div className='App list-group-item justify-content-center
+      aligh-items-center mx-auto' style={{"width":"400px",
+      "backgroundColor":"white", "marginTop":"15px"}}>
+        <h1 className="card text-white bg-primary mb-1"
+        styleName="max-width: 20rem;">GARBAGE LEVEL DETECTOR</h1>
+        <h6 className="card text-white bg-primary mb-3">
+          USING FARM STACK</h6>
+        <div className="card-body">
+          <h5 className="card text-white bg-dark mb-3">Add A Garbage Bin</h5>
+          <span className="card-text">
+            <input className="mb-2 form-control idIn" placeholder="ID"/>
+            <input className="mb-2 form-control desIn" placeholder="LEVEL"/>
+
+            <button className="btn btn-outline-primary mx-2 mb-3" style={
+            {'borderRadius':"50px", "font-weight":"bold"}}>Add Bin</button>
+          </span>
+
+          <h5 className="card text-white bg-dark mb-3">Your Bins</h5>
+          <div>
+            
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
