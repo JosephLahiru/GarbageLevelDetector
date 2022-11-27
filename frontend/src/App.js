a6e47c8cd225ca34fdbc44db504e3bb5d195ee0a
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2022-11-27 23:17:50 +0530

    minor

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -79,7 +79,7 @@ function App() {
         </span>
         <ToastContainer
               position="top-right"
-              autoClose={5000}
+              autoClose={3000}
               hideProgressBar={false}
               newestOnTop={false}
               closeOnClick
